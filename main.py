5cd3ef919ef43f97308be42981ab660b04fc8a87
Lucas Silva <lucassilvacabral7@gmail.com>
2025-03-28 10:10:48 -0300

    Commit inicial (caso base)

---
+++ main.py
@@ -0,0 +1,13 @@
+from mapas import mapa_incial
+from mapas import tela_inicial
+from personagens import nine
+from personagens import bozo
+from coletaveis import calango
+from coletaveis import joias
+from coletaveis import moeda
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
