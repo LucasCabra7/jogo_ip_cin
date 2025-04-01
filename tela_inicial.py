877061952670ab13e3f00cb0080bce4a6164b0f2
Lucas Silva <lucassilvacabral7@gmail.com>
2025-04-01 01:15:36 -0300

    Alterando imagem da tela inicial e adicionando música de fundo.

--- tela_inicial.py
+++ tela_inicial.py
@@ -10,17 +10,22 @@ def play2():  # Função que inicia o jogo quando clicar no botão "Jogar"
 def iniciar_tela():
     # Configuração da janela
     pygame.init()
+    pygame.mixer.init()
     window = pygame.display.set_mode([1280, 800])
     pygame.display.set_caption("Tela Inicial")
 
     # Carregar imagens
-    background = pygame.image.load("assets/capa.jpeg")
+    background = pygame.image.load("assets/CAPA.jpeg")
     button1_img = pygame.image.load("assets/botao_sair.png")
     button2_img = pygame.image.load("assets/botao_jogar.png")
 
     # Criar botões
-    button1 = botao.Button(0, 50, button1_img, 0.4)
-    button2 = botao.Button(950, 55, button2_img, 0.4)
+    button1 = botao.Button(650, 30, button1_img, 0.4)
+    button2 = botao.Button(950, 30, button2_img, 0.4)
+
+    pygame.mixer.music.load("assets/leila.mp3")
+    pygame.mixer.music.set_volume(0.5)
+    pygame.mixer.music.play(-1)
 
     loop = True
     while loop:
